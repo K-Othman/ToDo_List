a8f521b7a6155c3f315763557b7534d126748440
K-Othman <karim_othman88@hotmail.com>
2024-04-26 15:49:25 +0100

    created the header and the links

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header className="flex justify-between my-4">
+      <Link to="/">
+        <h1>ToDay</h1>
+      </Link>
+      <div className="flex gap-4">
+        <Link to="/login">
+          <h1>Login</h1>
+        </Link>
+        <Link to="/register">
+          <h1>Start for free</h1>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
