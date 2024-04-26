e17b1b5049c16a3d7203de7fdb44e63e2c226363
K-Othman <karim_othman88@hotmail.com>
2024-04-26 17:28:07 +0100

    handled the delete button correctly

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const Todos = () => {
   const [todos, setTodos] = useState([]);
+  const [value, setValue] = useState("");
   const theLink = "http://localhost:8800/api/";
 
   useEffect(() => {
@@ -17,22 +18,54 @@ const Todos = () => {
     fetchData();
   }, []);
 
+  //   const handleClick = async () => {
+  //     try {
+  //       const res = await axios.delete(`${theLink}todo/${_id}`);
+  //       //   setTodos(todos.filter((todo) => todo._id !== todoId));
+  //       console.log(res);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+
+  const handleClick = async (todoId) => {
+    try {
+      await axios.delete(`${theLink}todo/${todoId}`);
+      setTodos(todos.filter((todo) => todo._id !== todoId));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <section>
       <div>
         <form>
           <label htmlFor="todo">Todo : </label>
-          <input type="text" name="todo" placeholder="Todo" required />
+          <input
+            type="text"
+            name="todo"
+            placeholder="Todo"
+            value={value}
+            onChange={setValue}
+            required
+          />
         </form>
-        {todos.map((todo) => (
-          <div key={todo._id} className="flex justify-between">
-            <p>{todo.todo}</p>
-            <div className="flex gap-3">
-              <p>Delete</p>
-              <p>Update</p>
+        {todos ? (
+          todos.map((todo) => (
+            <div key={todo._id} className="flex justify-between">
+              <p>{todo.todo}</p>
+              <div className="flex gap-3">
+                <button onClick={() => handleClick(todo._id)}>Delete</button>
+
+                {/* <button onClick={handleClick}>Delete</button> */}
+                <p>Update</p>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p>What Are You Up to Today</p>
+        )}
       </div>
     </section>
   );
