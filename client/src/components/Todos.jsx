a05fe18b87ba4ab88f184b2f78d41eedbca41c9a
K-Othman <karim_othman88@hotmail.com>
2024-04-30 19:32:32 +0100

    styling the todos

--- client/src/components/Todos.jsx
+++ client/src/components/Todos.jsx
@@ -55,12 +55,13 @@ const Todos = () => {
   };
 
   return (
-    <section>
-      <div>
-        <form>
-          <label htmlFor="todo">
-            Todo :
+    <section className="text-center w-[700px] mx-auto mt-10">
+      <div className="">
+        <h1 className="font-bold mb-8 text-5xl">Todo List</h1>
+        <form className="h-10 flex items-center gap-1">
+          <label htmlFor="todo" className="flex-grow">
             <input
+              className="w-full outline-black p-1 mr-2 ml-0"
               type="text"
               name="todo"
               placeholder="Todo"
@@ -69,11 +70,18 @@ const Todos = () => {
               required
             />
           </label>
-          <input onClick={handlePost} type="submit" />
+          <input
+            className="w-40 h-8 bg-black text-white"
+            onClick={handlePost}
+            type="submit"
+          />
         </form>
         {todos.length > 0 ? (
           todos.map((todo) => (
-            <div key={todo._id} className="flex justify-between">
+            <div
+              key={todo._id}
+              className="flex justify-between my-1 bg-black text-white p-4"
+            >
               <div className="flex gap-3">
                 <input
                   type="checkbox"
@@ -86,12 +94,22 @@ const Todos = () => {
                 </p>
               </div>
               <div className="flex gap-3">
-                <button onClick={() => handleDelete(todo._id)}>Delete</button>
-                {/* <div onClick={() => handleUpdate(todo._id)}>
-                  {todo.done ? <p> Done</p> : <p>Not Done</p>}
-                </div> */}
-
-                {/* <button onClick={() => handleUpdate(todo._id)}>Update</button> */}
+                <button onClick={() => handleDelete(todo._id)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
               </div>
             </div>
           ))
